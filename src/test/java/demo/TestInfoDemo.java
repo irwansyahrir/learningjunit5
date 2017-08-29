00784019616212e180ae86e33c27f843c4081ad8
Irwan Syahrir <irwan.syahrir@gmail.com>
2017-08-29 16:03:03 +0200

    Added more demo tests.

---
+++ src/test/java/demo/TestInfoDemo.java
@@ -0,0 +1,36 @@
+package demo;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("TestInfo Demo")
+public class TestInfoDemo {
+
+    //TODO: find use cases for this feature. 1. Use it together with TestReporter. 2. Logging
+
+    TestInfoDemo(TestInfo testInfo) {
+        assertEquals("TestInfo Demo", testInfo.getDisplayName());
+    }
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        String displayName = testInfo.getDisplayName();
+        assertTrue(displayName.equals("TEST 1") || displayName.equals("test2()"));
+    }
+
+    @Test
+    @DisplayName("TEST 1")
+    @Tag("my-tag")
+    void test1(TestInfo testInfo) {
+        assertEquals("TEST 1", testInfo.getDisplayName());
+        assertTrue(testInfo.getTags().contains("my-tag"));
+        assertEquals("demo.TestInfoDemo", testInfo.getTestClass().get().getName());
+        assertEquals("test1", testInfo.getTestMethod().get().getName());
+    }
+
+    @Test
+    void test2() {
+    }
+}
---
+++ src/test/java/demo/TestReporterDemo.java
@@ -0,0 +1,36 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
+
+import java.util.HashMap;
+
+public class TestReporterDemo {
+
+    @Test
+    void reportSingleValue(TestReporter testReporter) {
+        testReporter.publishEntry("a key", "a value");
+    }
+
+    @Test
+    void reportSeveralValues(TestReporter testReporter) {
+        HashMap<String, String> values = new HashMap<>();
+        values.put("user name", "dk38");
+        values.put("award year", "1974");
+
+        testReporter.publishEntry(values);
+    }
+
+    //TODO: this looks more useful. Combined with TestInfo??
+    @Test
+    void reportSeveralValuesIncludingFromTestInfo(TestReporter testReporter, TestInfo testInfo) {
+        HashMap<String, String> values = new HashMap<>();
+        values.put("user name", "userName");
+        values.put("award year", "2017");
+        values.put("Test class name", testInfo.getTestClass().get().getName());
+
+        testReporter.publishEntry(values);
+
+    }
+}
