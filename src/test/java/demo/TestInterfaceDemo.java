12718d18e3436cce9c00d02408a52552a44d8065
Irwan Syahrir <irwan.syahrir@gmail.com>
2017-08-30 10:38:43 +0200

    Demo tests. Lifecycle

---
+++ src/test/java/demo/TestInterfaceDemo.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestInterfaceDemo implements TestLifecycleLogger,
+        TimeExecutionLogger, TestInterfaceDynamicTestsDemo {
+
+    //TODO: explore more use cases here. So far reporting has been the strong feature of JUnit5
+    //Can it be more meaningful for TDD? Readable test report while
+    //Or the reporting as documentation
+
+    @Test
+    void isEqualValue() {
+        assertEquals(1, 1, "is always equal");
+    }
+
+}
